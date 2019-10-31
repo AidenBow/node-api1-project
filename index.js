7a48c04fdb8ef89b882ce0d9368e96c044dd1257
AidenBow <AidenBow@users.noreply.github.com>
2019-10-31 13:36:56 -0700

    created boilerplate for index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// implement your API here
+const express = require('express');
+const db = require('./data/db');
+
+const server = express();
+
+server.listen("4000", () => {
+  console.log("()()() listening on port 4000 ()()()")
+})
+
+server.use(express.json());
+
+server.get("/api/users", (req, res) => {
+  res.send("hello")
+})
