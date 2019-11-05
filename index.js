f8a36c1fa6132b36d5c4b94eb7c7dcfd40a1e4f7
AidenBow <AidenBow@users.noreply.github.com>
2019-11-05 15:56:51 -0800

    added post user, get user, and get user by id

--- index.js
+++ index.js
@@ -9,6 +9,49 @@ server.listen("4000", () => {
 
 server.use(express.json());
 
-server.get("/api/users", (req, res) => {
-  res.send("hello")
+
+server.post("/api/users", (req, res) => {
+  const userInfo = req.body
+
+  db.insert(userInfo)
+    .then(user => {
+      res.status(201).json({success: true, user})
+    })
+    .catch(err => {
+      res.status(500).json({success: false, err})
+    })
 })
+
+server.get("/api/users", (req, res) => {
+  db.find()
+    .then(users => {
+      res.status(200).json(users)
+    })
+    .catch(err => {
+      res.status(500).json({success: false, err})
+    })
+})
+
+server.get(`/api/users/:id`, (req, res) => {
+  db.findById(req.params.id)
+    .then(user => {
+      if (user) {
+        res.status(200).json({success: true, user})
+      } else {
+        res.status(404).json({success: false, message: "The user with the specified ID does not exist."})
+      }
+    })
+    .catch(err => {
+      res.status(500).json({success: false, message: "server error"})
+    })
+})
+
+// server.put(`/api/users/:id`, (req, res) => {
+//   db.update(req.id, req.body)
+//     .then(user => {
+//       res.status(200).json({succes: true, user})
+//     })
+//     .catch => (err => {
+      
+//     })
+// })
